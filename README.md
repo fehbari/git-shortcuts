1c43defc2a7f1c1508220be5070a6e7519217200
Fernanda Bari <fehbari@gmail.com>
2021-04-30 05:08:20 -0300

    Fix #1 - Added documentation

--- README.md
+++ README.md
@@ -1,2 +1,151 @@
-# git-shortcuts
-Collection of useful Git shortcuts to call from Zsh.
+# git-shortcuts [![Platforms](https://img.shields.io/badge/platform-macos%20|%20linux-blue)]() [![Shell](https://img.shields.io/badge/shell-zsh-blue)]()
+
+## What is this?
+
+A collection of useful Git shortcuts to call from your Zsh terminal.
+
+## What does it do?
+
+It facilitates everyday Git tasks by providing a shorter syntax and convenient functions.
+
+## Not for you?
+
+These shortcuts were created to help my workflow. Most of them chain multiple Git commands in a way that saves me time. If it doesn't apply to your workflow, feel free to fork or tweak locally so it's useful to you.
+
+# Installation
+
+Download the [latest release](https://github.com/fehbari/git-shortcuts/releases/latest) and follow the instructions in that page.
+
+# Cheatsheet
+
+Type `g --help` to access this list at any time.
+
+Command               | Options       | Description
+--------------------- | ------------- | -----------
+[`gadd`](#gadd)       |               | Stage all new and changed files.
+[`gbase`](#gbase)     | `-s`          | Switch to base branch and optionally sync with remote.
+[`gbranch`](#gbranch) | `-n -p -d -c` | Switch to or perform actions on your branches.
+[`gcommit`](#gcommit) |               | Stage all changes and commit work with a message.
+[`gdiff`](#gdiff)     |               | List all staged and unstaged changes.
+[`gfetch`](#gfetch)   |               | Fetch branches and tags, and remove outdated local references.
+[`gmerge`](#gmerge)   |               | Merge latest from base branch into the active branch.
+[`gpull`](#gpull)     |               | *git pull* but shorter :)
+[`gpush`](#gpush)     | `-f`          | Push changes from active branch and track remote.
+[`grebase`](#grebase) | `-a -c -s`    | Rebase current branch onto base branch.
+[`gsquash`](#gsquash) |               | Use *git-squash* to squash commits and sync with base branch.
+[`gtrack`](#gtrack)   |               | Set the upstream of branch to origin/branch.
+
+# Reference
+
+Use `--help` with any command to see its detailed reference, or read it below.
+
+### gadd
+
+Stage all new and changed files.
+
+---
+
+### gbase
+
+Switch to base branch and optionally sync with remote.
+
+Option      | Description
+----------- | -----------
+`-s --sync` | Sync with latest changes from remote.
+
+---
+
+### gbranch
+
+Switch to or perform actions on your branches.
+
+**Basic use:**
+
+Switch to local *your/branch-name*:
+
+```
+gbranch your/branch-name
+```
+
+**Use with options:** 
+
+```
+gbranch [--options] your/branch-name
+```
+
+Option                 | Description
+---------------------- | -----------
+`-n --new`             | Create and switch to a new branch, based on the latest base branch.
+`-n -p --new --parent` | Create and switch to a new branch, based on the current branch (parent).
+`-t --track`           | Check out and track a remote branch locally.
+`-c --clean`           | Cleanup. Deletes all local branches except for base branch. </br> **Dangerous operation**, requires confirmation *(y/n)*
+
+---
+
+### gcommit
+
+Stage all changes and commit work with a message.
+
+**Usage:**
+
+```
+gcommit "Your commit message"
+```
+
+---
+
+### gdiff
+
+List all staged and unstaged changes.
+
+---
+
+### gfetch
+
+Fetch branches and tags, and remove outdated local references (prune).
+
+---
+
+### gmerge
+
+Merge latest from base branch into the active branch.
+
+---
+
+### gpull
+
+*git pull* but shorter :)
+
+---
+
+### gpush
+
+Push changes from active branch and track remote.
+
+Option       | Description
+------------ | -----------
+`-f --force` | Force push branch. Never do it on a shared branch! </br> **Dangerous operation**, requires confirmation *(y/n)*
+
+---
+
+### grebase
+
+Rebase current branch onto base branch.
+
+Option          | Description
+--------------- | -----------
+`-a --abort`    | Undo rebase with conflicts.
+`-c --continue` | Continue rebase after fixing conflicts.
+`-s --skip`     | Skip the current patch with conflicts.
+
+---
+
+### gsquash
+
+Use [git-squash](https://github.com/sheerun/git-squash) to squash commits and sync with base branch.
+
+---
+
+### gtrack
+
+Set the upstream of branch to *origin/branch*.
--- src/git-shortcuts.zsh
+++ src/git-shortcuts.zsh
@@ -1,38 +1,24 @@
 # Collection of useful Git shortcuts to call from Zsh.
-# Author: https://github.com/fehbari
+# Documentation: https://github.com/fehbari/git-shortcuts
 #
-# These are opinionated and catered to my workflow. Most of the shorcuts chain multiple
-# Git commands in a way I find useful. If it doesn't apply to your workflow, feel free 
-# to fork or tweak locally so it's useful to how you work.
-#
-# Prerequisites:
-# direnv - https://github.com/direnv/direnv (requires a BASE_BRANCH variable set)
-# git-squash - https://github.com/sheerun/git-squash (optional - only for gsquash command)
-#
-# Installation:
-# Copy this file to your home folder as ~/.git-shortcuts.zsh, then add `source ~/.git-shortcuts.zsh` 
-# in your ~/.zshrc file. With direnv installed, add the contents of the sample to your .envrc files.
-# When you're all set, call `g --help` to learn what you can do.
-#
-# Uninstall:
-# Remove the entry from your ~/.zshrc file and delete ~/.git-shortcuts.zsh from home folder.
+# Use `g --help` to learn what you can do.
 
 g() {
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
         echo "\nCollection of useful Git shortcuts.\n"
         echo "Available commands:\n"
-        echo "gadd | Stages all new and changed files."
-        echo "gbase [-s] | Switch to $BASE_BRANCH and optionally sync with latest."
+        echo "gadd | Stage all new and changed files."
+        echo "gbase [-s] | Switch to $BASE_BRANCH and optionally sync with remote."
         echo "gbranch [-n -p -d -c] | Switch to or perform actions on your branches."
         echo "gcommit | Stage all changes and commit work with a message."
-        echo "gdiff | Lists all staged and unstaged changes."
-        echo "gfetch | Fetch branches and tags, and removes outdated local references."
+        echo "gdiff | List all staged and unstaged changes."
+        echo "gfetch | Fetch branches and tags, and remove outdated local references."
         echo "gmerge | Merge latest from $BASE_BRANCH into the active branch."
         echo "gpull | git pull but shorter :)"
         echo "gpush [-f] | Push changes from active branch and track remote."
         echo "grebase [-a -c -s] | Rebase current branch onto $BASE_BRANCH."
         echo "gsquash | Use git-squash to squash commits and sync with $BASE_BRANCH."
-        echo "gtrack | Sets the upstream of branch to origin/branch."
+        echo "gtrack | Set the upstream of branch to origin/branch."
         echo "\nUse -h or --help with any command to see argument details."
     else
         echo "Collection of useful Git shortcuts to call from Zsh."
@@ -42,7 +28,7 @@ g() {
 
 gadd() {
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-        echo "\ngadd | Stages all new and changed files."
+        echo "\ngadd | Stage all new and changed files."
     else
         eval "git add ."
     fi
@@ -50,8 +36,8 @@ gadd() {
 
 gbase() {
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-        echo "\ngbase [-s] | Switch to $BASE_BRANCH and optionally sync with latest."
-        echo "\nAvailable arguments:\n"
+        echo "\ngbase [-s] | Switch to $BASE_BRANCH and optionally sync with remote."
+        echo "\nAvailable options:\n"
         echo "-s --sync | Sync with latest changes from origin/$BASE_BRANCH."
     elif [ "$1" = "-s" ] || [ "$1" = "--sync" ]; then
         eval "git checkout $BASE_BRANCH && git pull"
@@ -64,13 +50,13 @@ gbranch() {
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
         echo "\ngbranch [-n -p -d -c] | Switch to or perform actions on your branches."
         echo "\nBasic use: gbranch your/branch-name | Switch to local your/branch-name."
-        echo "\nAvailable arguments:\n"
+        echo "\nAvailable options:\n"
         echo "-n --new | Create and switch to a new branch, based on the latest $BASE_BRANCH."
         echo "-n -p --new --parent | Create and switch to a new branch, based on the current branch (parent)."
         echo "-t --track | Check out and track a remote branch locally."
         echo "-c --clean | Cleanup. Deletes all local branches except for $BASE_BRANCH."
         echo "             Dangerous operation, requires confirmation (y/n)"
-        echo "\nUsage: gbranch [--args] your/branch-name"
+        echo "\nUsage: gbranch [--options] your/branch-name"
     elif [ "$1" = "-n" ] || [ "$1" = "--new" ]; then
         if [ "$2" = "-p" ] || [ "$2" = "--parent" ]; then
             eval "git checkout -b $3"
@@ -104,7 +90,7 @@ gcommit() {
 
 gdiff() {
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-        echo "\ngdiff | Lists all staged and unstaged changes."
+        echo "\ngdiff | List all staged and unstaged changes."
     else
         eval "git diff HEAD --name-status && echo \"\n\""
         eval "git diff --stat HEAD"
@@ -113,7 +99,7 @@ gdiff() {
 
 gfetch() {
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-        echo "\ngfetch | Fetch branches and tags, and removes outdated local references."
+        echo "\ngfetch | Fetch branches and tags, and remove outdated local references (prune)."
     else
         eval "git fetch --prune"
     fi
@@ -138,7 +124,7 @@ gpull() {
 gpush() {
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
         echo "\ngpush [-f] | Push changes from active branch and track remote."
-        echo "\nAvailable arguments:\n"
+        echo "\nAvailable options:\n"
         echo "-f --force | Force push branch. Never do it on a shared branch!"
         echo "             Dangerous operation, requires confirmation (y/n)"
     elif [ "$1" = "-f" ] || [ "$1" = "--force" ]; then
@@ -156,7 +142,7 @@ gpush() {
 grebase() {
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
         echo "\ngrebase [-a -c -s] | Rebase current branch onto $BASE_BRANCH."
-        echo "\nAvailable arguments:\n"
+        echo "\nAvailable options:\n"
         echo "-a --abort | Undo rebase with conflicts."
         echo "-c --continue | Continue rebase after fixing conflicts."
         echo "-s --skip | Skip the current patch with conflicts."
@@ -181,7 +167,7 @@ gsquash() {
 
 gtrack() {
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-        echo "\ngtrack | Sets the upstream of branch to origin/branch."
+        echo "\ngtrack | Set the upstream of branch to origin/branch."
     else
         branch=$(git symbolic-ref --short HEAD)
         eval "git branch --set-upstream-to origin/$branch"
